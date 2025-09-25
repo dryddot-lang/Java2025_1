83f39e8e54d22772c76931cb8930febc2f0f2eb3
TimofeyImaev <135110212+TimofeyImaev@users.noreply.github.com>
2025-09-25 16:14:15 +0500

    Added BMI counter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter weight in kg: ");
+        double weight = scanner.nextDouble();
+        System.out.print("Enter height in cm: ");
+        double height = scanner.nextDouble();
+        height /= 100;
+
+        double bmi = weight / (height * height);
+
+        String ans;
+        if (bmi < 18.5) ans = "Underweight";
+        else if (18.5 <= bmi && bmi < 25.0) ans = "norm";
+        else if (25.0 <= bmi && bmi < 30.0) ans = "Overweight";
+        else ans = "fatness";
+
+        System.out.printf("Your BMI: %.1f. You have %s", bmi, ans);
+
+        scanner.close();
     }
 }
